89de2a0e485e428a78c62386ef75f08eb7adbe9f
Antonin-Nosek <antonin.nosek.024@pslib.cz>
2025-05-09 12:56:28 +0200

    ovladac pro pwcar

--- main.ts
+++ main.ts
@@ -1,1 +1,13 @@
+radio.setGroup(1);
 
+basic.forever(function () {
+    let x = input.acceleration(Dimension.X); //left,right
+    let y = input.acceleration(Dimension.Y); //forward,backwards
+    radio.sendValue("x", x);
+    radio.sendValue("y", y);
+
+    basic.pause(100);
+})
+input.onButtonPressed(Button.A, function () {
+    radio.sendString("Stop");
+})
