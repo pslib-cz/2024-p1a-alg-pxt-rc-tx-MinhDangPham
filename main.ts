2c2953d6d8ae2281551230d792fca58e079e3b72
Minh Dang Pham <minh.pham.024@pslib.cz>
2025-05-09 18:03:45 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,11 @@
+let y = 0
+let x = 0
+radio.setGroup(11)
+basic.forever(function () {
 
+    x = input.acceleration(Dimension.X)
+    y = input.acceleration(Dimension.Y)
+    radio.sendValue("x", x)
+    radio.sendValue("y", y)
+    basic.pause(100)
+})
