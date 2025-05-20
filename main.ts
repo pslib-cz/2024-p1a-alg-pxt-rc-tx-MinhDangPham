6d791c069b653aa882ff0088c2726ae42557ae72
Minh Dang Pham <minh.pham.024@pslib.cz>
2025-05-20 20:00:41 +0200

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,15 @@
-radio.setGroup(11);
+radio.setGroup(11)
 
 basic.forever(function () {
-    let x = input.acceleration(Dimension.X);
-    let y = input.acceleration(Dimension.Y);
-    radio.sendValue("x", x);
-    radio.sendValue("y", y);
+    let x = input.acceleration(Dimension.X)
+    let y = input.acceleration(Dimension.Y)
 
-    basic.pause(100);
+    // Send x and y as a single string separated by a comma
+    radio.sendString(`${x},${y}`)
+
+    basic.pause(100)
 })
+
 input.onButtonPressed(Button.A, function () {
-    radio.sendString("Stop");
+    radio.sendString("Stop")
 })
