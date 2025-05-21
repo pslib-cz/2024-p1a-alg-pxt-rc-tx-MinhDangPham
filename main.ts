857ddafefefbbc9c2377eced9ecd335d96905cd6
Minh Dang Pham <minh.pham.024@pslib.cz>
2025-05-21 08:16:35 +0200

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -4,7 +4,6 @@ basic.forever(function () {
     let x = input.acceleration(Dimension.X)
     let y = input.acceleration(Dimension.Y)
 
-    // Send x and y as a single string separated by a comma
     radio.sendString(`${x},${y}`)
 
     basic.pause(100)
